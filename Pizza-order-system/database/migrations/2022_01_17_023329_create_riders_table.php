c75c1619ea18794070c7e9bdeea4e5ede5e80f09
Ye Wai <yewai.cs@gmail.com>
2022-01-17 09:47:22 +0630

    create migratations

--- Pizza-order-system/database/migrations/2022_01_17_023329_create_riders_table.php
+++ Pizza-order-system/database/migrations/2022_01_17_023329_create_riders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateRidersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('riders', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('phone');
+            $table->string('phone', 50);
+            $table->string('email', 100);
             $table->string('address');
-            $table->string('password');
-            $table->string('role');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -34,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('riders');
     }
 }
