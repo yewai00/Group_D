69b04ee415b6b04c83ad458ea31f839f765ef9f1
nandaroo <nandaroo279230@gmail.com>
2022-01-24 09:39:49 +0630

    [develop] added db transcation

--- Pizza-order-system/app/Http/Controllers/CategoryController.php
+++ Pizza-order-system/app/Http/Controllers/CategoryController.php
@@ -31,7 +31,7 @@ class CategoryController extends Controller
     public function index()
     {
         $categories = $this->categoryInterface->getCategoriesList();
-        return view('Admin.category.index')
+        return view('Admin.Category.index')
             ->with('categories', $categories);
     }
 
@@ -53,7 +53,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        return view('Admin.category.create');
+        return view('Admin.Category.create');
     }
 
     /**
@@ -78,7 +78,7 @@ class CategoryController extends Controller
     public function edit($id)
     {
         $category = $this->categoryInterface->edit($id);
-        return view('Admin.category.edit')
+        return view('Admin.Category.edit')
             ->with('category', $category);
     }
 
@@ -104,7 +104,7 @@ class CategoryController extends Controller
     public function search(Request $request)
     {
         $categories = $this->categoryInterface->search($request);
-        return view('Admin.category.index')
+        return view('Admin.Category.index')
             ->with('categories', $categories);
     }
 
