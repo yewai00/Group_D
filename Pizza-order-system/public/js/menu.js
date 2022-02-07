531b9fb4b727d7a4d14f29eefd21208c99553e0e
nandaroo <nandaroo279230@gmail.com>
2022-02-07 08:32:06 +0630

    [develop] pagination js

--- Pizza-order-system/public/js/menu.js
+++ Pizza-order-system/public/js/menu.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
     $('.humburger').click(function() {
       $(this).toggleClass('active');
-  
+
       if ($(this).hasClass('active')) {
           $('.nav-inner').addClass('active');
       } else {
           $('.nav-inner').removeClass('active');
       }
     });
+
+    $('.page-link').attr("href", function()
+    {
+      return $(this).attr("href") + "#pizza-list"
+      });
   });//menu JS
